25005ddb575efadcc5027a9f8e99a2dcfb1ea99f
hadron43 <harsh19043@iiitd.ac.in>
2020-04-05 09:14:22 +0530

    Completed Week1 Question 5
    Completed Week 1 Assignment 5/5. Meanwhile, I have also completed
    week 2 question 1 in previous commit.

--- DataStructures/week1_basic_data_structures/MaxSlidingWindow.java
+++ DataStructures/week1_basic_data_structures/MaxSlidingWindow.java
@@ -5,8 +5,7 @@ class MaxSlidingWindow{
     public static void main(String[] args) throws IOException{
         FastScanner sc = new FastScanner();
 
-        Queue<Integer> stack = new LinkedList<>();
-        Queue<Integer> max = new LinkedList<>();
+        Deque<Integer> Q = new LinkedList<Integer>();
 
         int n = sc.nextInt();
         int a[] = new int[n];
@@ -14,9 +13,53 @@ class MaxSlidingWindow{
             a[i] = sc.nextInt();
         
         int m = sc.nextInt();
+
+        //Process the first k elements
+        for(int i=0; i<m; ++i){
+            while(!Q.isEmpty() && a[Q.peekLast()]<a[i])
+                Q.removeLast();
+            
+            Q.addLast(i);
+        }
+        //Print Asnwer for First Window
+        System.out.print(a[Q.peekFirst()]+" ");
+
+        //Slide the Window and print the max element
+        for(int i=m; i<n; ++i){
+            while(!Q.isEmpty() && a[Q.peekLast()]<a[i])
+                Q.removeLast();
+            
+            if(!Q.isEmpty() && Q.peekFirst()<=i-m)
+                Q.removeFirst();
+            
+            Q.addLast(i);
+
+            System.out.print(a[Q.peekFirst()]+" ");
+        }
         
     }
         
+}
+
+class FastScanner {
+    private BufferedReader reader;
+    private StringTokenizer tokenizer;
+
+    public FastScanner() {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        tokenizer = null;
+    }
+
+    public String next() throws IOException {
+        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            tokenizer = new StringTokenizer(reader.readLine());
+        }
+        return tokenizer.nextToken();
+    }
+
+    public int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
 }
 /*
 8
