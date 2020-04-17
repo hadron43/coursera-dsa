36d94bbe357fd3bb2a57bcfd9e667be26f6f6e73
hadron43 <harsh19043@iiitd.ac.in>
2020-04-18 01:04:10 +0530

    Corrected my solution for Week 5 - Task 2 PrimitiveCalculator
    Previous solution was comparitively slower (though asymptocially correct)
    I modified my code for faster execution. I removed unnecessary use of
    Node class and used an array instead.
    
    I basically stopped storing the backtracking part in memory. I decided
    my path while backtracking itself.

--- AlgorithmicToolbox/week5_dynamic_programming1/PrimitiveCalculator.java
+++ AlgorithmicToolbox/week5_dynamic_programming1/PrimitiveCalculator.java
@@ -4,40 +4,41 @@ public class PrimitiveCalculator {
     private static List<Integer> optimal_sequence(int n) {
         List<Integer> sequence = new ArrayList<Integer>();
         
-        Node numbers[] = new Node[n];
-        numbers[0] = new Node(0, 0);
+        int numbers[] = new int[n];
+        numbers[0] = 0;
 
-        int op, noOfSteps;
+        int noOfSteps;
         int ans[] = new int[3];
         for(int i=2; i<=n; ++i){
             ans[0] = ans[1] = ans[2] = Integer.MAX_VALUE;
 
-            ans[0] = numbers[(i-1)-1].noOfSteps + 1;
+            ans[0] = numbers[(i-1)-1] + 1;
             if(i%2==0)
-                ans[1] = numbers[i/2 - 1].noOfSteps + 1;
+                ans[1] = numbers[i/2 - 1] + 1;
 
             if(i%3==0)
-                ans[2] = numbers[i/3 - 1].noOfSteps + 1;
+                ans[2] = numbers[i/3 - 1] + 1;
             
             if(ans[0]<ans[1] && ans[0]<ans[2]){
-                op = 1;
                 noOfSteps = ans[0];
             }
             else if(ans[1]<ans[2]){
-                op = 2;
                 noOfSteps = ans[1];
             }
             else{
-                op = 3;
                 noOfSteps = ans[2];
             }
 
-            numbers[i-1] = new Node(op, noOfSteps);
+            numbers[i-1] = noOfSteps;
         }
 
-        int temp = n;
+        int temp = n, op;
         while(temp>1){
-            op = numbers[temp-1].op;
+            op = 1;
+            if(temp%3==0)
+                op = 3;
+            else if(temp%2==0 && numbers[temp/2 - 1]<=numbers[temp-1 - 1])
+                op = 2;
             sequence.add(temp);
             if(op == 1)
                 temp-=1;
@@ -60,15 +61,5 @@ public class PrimitiveCalculator {
         for (Integer x : sequence) {
             System.out.print(x + " ");
         }
-    }
-}
-
-class Node{
-    int op;
-    int noOfSteps;
-
-    Node(int op, int noOfSteps){
-        this.op = op;
-        this.noOfSteps = noOfSteps;
     }
 }
