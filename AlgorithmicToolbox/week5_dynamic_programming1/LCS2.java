a39a28e0971dd9ea164757677b5ce427991c12d6
hadron43 <harsh19043@iiitd.ac.in>
2020-04-19 22:24:36 +0530

    Completed Task 4 Week 5 - Longest Common Subsequence of Two Array
    I completed the task using DP and removed unnecessary files.

--- AlgorithmicToolbox/week5_dynamic_programming1/LCS2.java
+++ AlgorithmicToolbox/week5_dynamic_programming1/LCS2.java
@@ -3,8 +3,24 @@ import java.util.*;
 public class LCS2 {
 
     private static int lcs2(int[] a, int[] b) {
-        //Write your code here
-        return Math.min(a.length, b.length);
+        int dp[][] = new int[a.length+1][b.length+1];
+        dp[0][0] = 0;
+
+        for(int i=1; i<=a.length; ++i)
+            dp[i][0] = 0;
+        for(int i=1; i<=b.length; ++i)
+            dp[0][i] = 0;
+
+        for(int i=1; i<=a.length; ++i){
+            for(int j=1; j<=b.length; ++j){
+                if(a[i-1]==b[j-1])
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                else
+                    dp[i][j] = Integer.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        return dp[a.length][b.length];
     }
 
     public static void main(String[] args) {
