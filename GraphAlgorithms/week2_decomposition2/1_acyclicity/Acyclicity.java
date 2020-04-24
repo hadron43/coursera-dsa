f786bb502812f0e3560ddb03cdf206028ff4d1e8
hadron43 <harsh19043@iiitd.ac.in>
2020-04-24 16:51:51 +0530

    Completed Week 2 Task 1 - Acyclicity
    Completed this with keeping track of current elements in the call stack
    by passing a copy of the elements visited to further recursive call in
    this call frame.
    
    I am not confident if the solution that I did was the best possible solution,
    I will look for better implementation for the same problem and update
    if found something interesting.

--- GraphAlgorithms/week2_decomposition2/1_acyclicity/Acyclicity.java
+++ GraphAlgorithms/week2_decomposition2/1_acyclicity/Acyclicity.java
@@ -2,8 +2,40 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Acyclicity {
+
+    static boolean visited[];
+
+	private static boolean explore(ArrayList<Integer>[] adj, int i, ArrayList<Integer> curr){
+        if(curr.contains(i)){
+            return false;
+        }
+        curr.add(i);
+
+        if(visited[i]==true)
+            return true;
+        visited[i] = true;
+        
+		boolean ans = true;
+		for(int j: adj[i]){
+			ans = ans & explore(adj, j, new ArrayList<>(curr));
+		}
+		return ans;
+		
+	}
+
     private static int acyclic(ArrayList<Integer>[] adj) {
-        //write your code here
+        int n = adj.length;
+        visited = new boolean[n];
+        boolean ans = true;
+        for(int i=0; i<n; ++i){
+        	if(visited[i]==false){
+                ans = ans & explore(adj, i,  new ArrayList<Integer>());
+            }
+
+        	if(ans==false)
+        		return 1;
+        }
+
         return 0;
     }
 
