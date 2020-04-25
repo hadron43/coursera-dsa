52ea048e129158a148a642fa57ee347f646f660c
hadron43 <harsh19043@iiitd.ac.in>
2020-04-26 00:11:47 +0530

    Completed Week 4 Task 1 - Dijkstra's Algorithm for Shortest Path
    Completed the task and removed starter files for other languages.

--- GraphAlgorithms/week4_paths2/1_dijkstra/Dijkstra.java
+++ GraphAlgorithms/week4_paths2/1_dijkstra/Dijkstra.java
@@ -2,7 +2,44 @@ import java.util.*;
 
 public class Dijkstra {
     private static int distance(ArrayList<Integer>[] adj, ArrayList<Integer>[] cost, int s, int t) {
-        return -1;
+        int n = adj.length;
+        int dist[] = new int[n];
+            
+        for(int i=0; i<n; ++i){
+            dist[i] = Integer.MAX_VALUE;
+        }
+
+        dist[s] = 0;
+        PriorityQueue<Node> q = new PriorityQueue(new Comp());
+        q.add(new Node(s, 0));
+
+        boolean done[] = new boolean[n];
+
+        while(q.size()>0){
+            Node temp = q.poll();
+            
+            if(done[temp.index]==true)
+                continue;
+
+            int x = temp.index, xdist = temp.dist;
+            for(int i=0; i<adj[x].size(); ++i){
+                int aIndex = adj[x].get(i);
+
+                if(dist[aIndex]==Integer.MAX_VALUE)
+                    q.add(new Node(aIndex, dist[aIndex]));
+
+                if(xdist + cost[x].get(i) < dist[aIndex]){
+                    dist[aIndex] = xdist + cost[x].get(i);
+                    q.add(new Node(aIndex, dist[aIndex]));
+                }
+            }
+            done[x] = true;
+        }
+
+        if(dist[t]<0 || dist[t]==Integer.MAX_VALUE)
+            return -1;
+        
+        return dist[t];
     }
 
     public static void main(String[] args) {
@@ -29,3 +66,24 @@ public class Dijkstra {
     }
 }
 
+class Node{
+    int index, dist;
+
+    Node(int index, int dist){
+        this.index = index;
+        this.dist = dist;
+    }
+}
+
+class Comp implements Comparator<Node>{
+    @Override
+    public int compare(Node arg0, Node arg1) {
+        if(arg0.dist<arg1.dist)
+            return -1;
+
+        if(arg1.dist<arg0.dist)
+            return 1;
+
+        return 0;
+    }
+}
