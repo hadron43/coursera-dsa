5e5cb296134873c866b31ac003de302de479b43d
hadron43 <harsh19043@iiitd.ac.in>
2020-04-25 16:01:35 +0530

    Completed Week 2 Task 3 - Strongly Connected components
    I completed this task and removed starter files for other languages.

--- GraphAlgorithms/week2_decomposition2/3_strongly_connected/StronglyConnected.java
+++ GraphAlgorithms/week2_decomposition2/3_strongly_connected/StronglyConnected.java
@@ -4,8 +4,64 @@ import java.util.Scanner;
 
 public class StronglyConnected {
     private static int numberOfStronglyConnectedComponents(ArrayList<Integer>[] adj) {
-        //write your code here
-        return 0;
+        ArrayList<Integer> revAdj[] = (ArrayList<Integer>[]) new ArrayList[adj.length];
+        int ans = 0;
+        for (int i = 0; i < adj.length; i++) {
+            revAdj[i] = new ArrayList<Integer>();
+        }
+
+        for(int i=0; i<adj.length; ++i){
+            for(int x: adj[i])
+                revAdj[x].add(i);
+        }
+
+        deleted = new int[adj.length];
+        int post[] = new int[adj.length];
+        int used[] = new int[adj.length];
+
+        counter = 0;
+        for(int i=0; i<adj.length; ++i){
+            if(used[i]==0)
+                dfs(revAdj, post, used, i);
+        }
+
+        while(noOfDeletions<adj.length){
+            int highestPost = -1;
+            for(int i=0; i<post.length; ++i){
+                if(deleted[i]==1)
+                    continue;
+
+                if(highestPost==-1){
+                    highestPost = i;
+                    continue;
+                }
+
+                if(post[i]>post[highestPost])
+                    highestPost = i;
+
+            }
+            delete(adj, highestPost);
+            ans++;
+        }
+        return ans;
+    }
+    static int counter, deleted[];
+    public static void dfs(ArrayList<Integer>[] adj, int[] post, int[] used, int x){
+        used[x] = 1;
+        for(int i: adj[x])
+            if(used[i]==0)
+                dfs(adj, post, used, i);
+
+        post[x] = ++counter;
+    }
+    static int noOfDeletions;
+
+    static void delete(ArrayList<Integer>[] adj, int highestPost){
+        deleted[highestPost] = 1;
+        noOfDeletions++;
+        for(int i: adj[highestPost])
+            if(deleted[i]==0)
+                delete(adj, i);
     }
 
     public static void main(String[] args) {
