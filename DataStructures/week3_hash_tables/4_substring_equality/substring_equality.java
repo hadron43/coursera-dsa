52b66f8b734f81296d68937d26da3a828111ce7d
hadron43 <harsh19043@iiitd.ac.in>
2020-04-30 22:19:32 +0530

    Completed Week 4 Task 4 - Substring Equality
    Completed the task and removed starter files for other languages.

--- DataStructures/week3_hash_tables/4_substring_equality/substring_equality.java
+++ DataStructures/week3_hash_tables/4_substring_equality/substring_equality.java
@@ -2,13 +2,48 @@ import java.util.*;
 import java.io.*;
 
 public class substring_equality {
-	public class Solver {
+	static public class Solver {
 		private String s;
+		private static long m1 = 1000000007, m2 = 1000000009;
+		private long x;
+		private long[] h1, h2;
+		private long[] pow1, pow2;
 		public Solver(String s) {
 			this.s = s;
+			this.x = (long)(Math.random()*1000000000);
+			// this.x = 1;
+			
+			int n = s.length();
+			h1 = new long[n + 1];
+			h2 = new long[n + 1];
+
+			h1[0] = h2[0] = 0;
+			for(int i=1; i<=n; ++i){
+				h1[i] = ((x*h1[i-1] + s.charAt(i-1)) % m1 + m1)%m1;
+				h2[i] = ((x*h2[i-1] + s.charAt(i-1)) % m2 + m2)%m2;
+			}
+
+			pow1 = new long[n+1];
+			pow2 = new long[n+1];
+			pow1[0] = pow2[0] = 1;
+			for(int i=1; i<=n; ++i){
+				pow1[i] = (pow1[i-1] * x%m1)%m1;
+				pow2[i] = (pow2[i-1] * x%m2)%m2;
+			}
 		}
 		public boolean ask(int a, int b, int l) {
-			return s.substring(a, a + l).equals(s.substring(b, b + l));
+			long checka, checkb;
+			checka = ((h1[a+l] - pow1[l] * h1[a])%m1 + m1)%m1;
+			checkb = ((h1[b+l] - pow1[l] * h1[b])%m1 + m1)%m1;
+
+			if(checka==checkb){
+				checka = ((h2[a+l] - pow2[l] * h2[a])%m2 + m2)%m2;
+				checkb = ((h2[b+l] - pow2[l] * h2[b])%m2 + m2)%m2;
+
+				if(checka==checkb)
+					return true;
+			}
+			return false;
 		}
 	}
 
