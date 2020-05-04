adde4bad5d830684f30db46f24b3feed7f036749
hadron43 <harsh19043@iiitd.ac.in>
2020-05-04 14:48:56 +0530

    Completed Week 4 Task 1 - Tree Orders
    Completed the tree traversals in Data Structures, and removed unnecessary
    starter files.

--- DataStructures/week4_binary_search_trees/1_tree_traversals/tree_orders.java
+++ DataStructures/week4_binary_search_trees/1_tree_traversals/tree_orders.java
@@ -38,27 +38,54 @@ public class tree_orders {
 			}
 		}
 
+		void in(int root, ArrayList<Integer> result){
+			if(root<0)
+				return;
+			
+			in(left[root], result);
+			result.add(key[root]);
+			in(right[root], result);
+		}		
+
 		List<Integer> inOrder() {
 			ArrayList<Integer> result = new ArrayList<Integer>();
                         // Finish the implementation
                         // You may need to add a new recursive method to do that
-                        
+            in(0, result);            
 			return result;
 		}
+
+		void pre(int root, ArrayList<Integer> result){
+			if(root<0)
+				return;
+			
+			result.add(key[root]);
+			pre(left[root], result);
+			pre(right[root], result);
+		}		
 
 		List<Integer> preOrder() {
 			ArrayList<Integer> result = new ArrayList<Integer>();
                         // Finish the implementation
                         // You may need to add a new recursive method to do that
-                        
+            pre(0, result);            
 			return result;
 		}
+
+		void post(int root, ArrayList<Integer> result){
+			if(root<0)
+				return;
+			
+			post(left[root], result);
+			post(right[root], result);
+			result.add(key[root]);
+		}	
 
 		List<Integer> postOrder() {
 			ArrayList<Integer> result = new ArrayList<Integer>();
                         // Finish the implementation
                         // You may need to add a new recursive method to do that
-                        
+            post(0, result);            
 			return result;
 		}
 	}
@@ -89,3 +116,20 @@ public class tree_orders {
 		print(tree.postOrder());
 	}
 }
+
+
+/*abstract
+
+10
+0 7 2
+10 -1 -1
+20 -1 6
+30 8 9
+40 3 -1
+50 -1 -1
+60 1 -1
+70 5 4
+80 -1 -1
+90 -1 -1
+
+*/
