77cd01ba2619a2220bf45de3a6ce52c09eca69ed
Harsh Kumar <harsh19043@iiitd.ac.in>
2020-05-23 13:24:08 +0530

    Completed Week 4 Task 2 - Detecting Anomalies in Currency Exchange Rate
    Completed the task in Java and removed starter files for other languages.

--- GraphAlgorithms/week4_paths2/2_negative_cycle/NegativeCycle.java
+++ GraphAlgorithms/week4_paths2/2_negative_cycle/NegativeCycle.java
@@ -1,9 +1,72 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class NegativeCycle {
+
+    private static int dfs(ArrayList<Integer>[] adj, int s, boolean visited[]){
+        if(visited[s])
+            return -1;
+
+        visited[s] = true;
+        for(int i: adj[s]){
+            dfs(adj, i, visited);
+        }    
+
+        return s;
+    }
+
+    private static int checkComponent(ArrayList<Integer>[] adj, ArrayList<Integer>[] cost, int mother){
+        int N = adj.length;
+        int dist[] = new int[N];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        dist[mother] = 0;
+
+        for(int i=0; i<N-1; ++i){
+            boolean noRelax = true;
+            for(int u=0; u<N; ++u){
+                for(int j=0; j<adj[u].size(); ++j){
+                    int v = adj[u].get(j);
+                    if(dist[u]!=Integer.MAX_VALUE && dist[u]+cost[u].get(j)<dist[v]){
+                        dist[v] = dist[u]+cost[u].get(j);
+                        noRelax = false;
+                    }
+                }
+            }
+            if(noRelax)
+                break;
+        }
+
+        boolean noRelax = true;
+        for(int u=0; u<N; ++u){
+            for(int j=0; j<adj[u].size(); ++j){
+                int v = adj[u].get(j);
+                if(dist[u]!=Integer.MAX_VALUE && dist[u]+cost[u].get(j)<dist[v]){
+                    dist[v] = dist[u]+cost[u].get(j);
+                    noRelax = false;
+                }
+            }
+        }
+
+        if(noRelax)
+            return 0;
+
+        return 1;
+    }
+
     private static int negativeCycle(ArrayList<Integer>[] adj, ArrayList<Integer>[] cost) {
-        // write your code here
+        int N = adj.length;
+        boolean visited[] = new boolean[N];
+        for(int i=0; i<N; ++i){
+            int temp = dfs(adj, i, visited);
+
+            if(temp!=-1){
+                if(checkComponent(adj, cost, temp)==1)
+                    return 1;
+            }
+        }
+
         return 0;
     }
 
@@ -26,6 +89,7 @@ public class NegativeCycle {
             cost[x - 1].add(w);
         }
         System.out.println(negativeCycle(adj, cost));
+        scanner.close();
     }
 }
 
